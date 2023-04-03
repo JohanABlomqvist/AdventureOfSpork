f91999343da92d282672cbc5ea8fc8f5070ea787
JohanABlomqvist <Therionpoe@gmail.com>
2023-04-03 17:16:43 +0000

    fix to code because of 80 character constraint

--- run.py
+++ run.py
@@ -13,8 +13,18 @@ def get_row(row_number):
         service = get_google_sheets_api()
         sheet_id = '1y1gpQaX6n1Pix4pcw7K_MTS8X1hg_703FGEO3Y4Ll3w'
         range_name = f'Sheet1!A{row_number}:H{row_number}'
-        result = service.spreadsheets().values().get(spreadsheetId=sheet_id, range=range_name).execute()
+        result = (
+            service.spreadsheets().values().get(
+                spreadsheetId=sheet_id, range=range_name).execute()
+        )
         return result['values'][0]
     except HttpError as error:
         print(f"An error occurred: {error}")
         return None
+
+
+def display_dialogue(character, dialogue, options):
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print(f"{character}: {dialogue}")
+    for i, option in enumerate(options, 1):
+        print(f"({i}) {option}")
