a557aee48c10836235ae1155d206016139bf4899
JohanABlomqvist <Therionpoe@gmail.com>
2023-04-03 12:01:16 +0000

    forgot to safe before push

--- run.py
+++ run.py
@@ -1,3 +1,8 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import curses
+
+# Set up the screen
+screen = curses.initscr()
+curses.noecho()
+curses.cbreak()
+screen.keypad(True)
+
