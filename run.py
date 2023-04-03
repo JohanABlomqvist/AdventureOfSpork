b1b68458758239f931c940ef0f84b68d2cec26a3
JohanABlomqvist <Therionpoe@gmail.com>
2023-04-03 17:03:40 +0000

    adding google sheets apicode to python, not done

--- run.py
+++ run.py
@@ -2,3 +2,8 @@ import os
 import google.auth
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
+
+def get_google_sheets_api():
+    creds, _ = google.auth.default()
+    service = build('sheets', 'v4', credentials=creds, cache_discovery=False)
+    return service
