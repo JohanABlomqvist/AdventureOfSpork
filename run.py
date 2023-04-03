548d2bfc90e08ee44ddf4207f23f8170793b3431
JohanABlomqvist <Therionpoe@gmail.com>
2023-04-03 12:32:59 +0000

    set up colors and function for centered text

--- run.py
+++ run.py
@@ -6,7 +6,12 @@ curses.noecho()
 curses.cbreak()
 screen.keypad(True)
 
+# Set up colors
+curses.start_color()
+curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLUE)
+curses.init_pair(2, curses.COLOR_BLACK, curses.COLOR_WHITE)
+
 #Defining function to print centered text
 def prin_centered(y, text, color_pair):
     x = int ((80 - len(text)) / 2)
-    
+    screen.addstr(y, x, text, color_pair)
