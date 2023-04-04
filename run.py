7c05406f4418212182f0c1bee08075610caaae79
JohanABlomqvist <Therionpoe@gmail.com>
2023-04-04 09:38:26 +0000

    trying to add visuals to code

--- run.py
+++ run.py
@@ -1,10 +1,11 @@
 import os
-import google.auth
+from google.oauth2 import service_account
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
 def get_google_sheets_api():
-    creds, _ = google.auth.default()
+    key_file_path = os.environ['GOOGLE_APPLICATION_CREDENTIALS']
+    creds = service_account.Credentials.from_service_account_file(key_file_path)
     service = build('sheets', 'v4', credentials=creds, cache_discovery=False)
     return service
 
@@ -22,9 +23,32 @@ def get_row(row_number):
         print(f"An error occurred: {error}")
         return None
 
+def display_visuals(current_row):
+    if current_row in range(2, 5):
+        print("===== Forest =====")
+        print("  .  /\\")
+        print(" *- /  \\")
+        print("    /\\  \\")
+        print("   /  \\  \\")
+        print("  /    \\  \\")
+        print("===================")
+    elif current_row in range(5, 9):
+        print("======= Tavern =======")
+        print("  __[___]__")
+        print("  |       |")
+        print("  |  |L|  |")
+        print("  |__|_|__|")
+        print("=======================")
+    elif current_row in range(9, 13):
+        print("===== Goblin King =====")
+        print("  (\\_/)")
+        print(" (*)_(*_)")
+        print("  (/   \\)")
+        print("========================")
 
-def display_dialogue(character, dialogue, options):
+def display_dialogue(character, dialogue, options, current_row):
     os.system('cls' if os.name == 'nt' else 'clear')
+    display_visuals(current_row)
     print(f"{character}: {dialogue}")
     for i, option in enumerate(options, 1):
         print(f"({i}) {option}")
@@ -41,7 +65,7 @@ def play_game():
         options = [option for option in [option_a, option_b, option_c] if option]
         next_rows = [int(next_row) for next_row in [next_a, next_b, next_c] if next_row]
 
-        display_dialogue(character, dialogue, options)
+        display_dialogue(character, dialogue, options, current_row)
 
         if current_row in [10, 11, 12]:  # Check if the game is finished
             print("You finished the game!")
@@ -49,7 +73,10 @@ def play_game():
 
         while True:
             user_choice = input("Choose an option (1, 2, or 3): ")
-            if user_choice.isdigit() and 1 <= int(user_choice) <= len(options):
+            if (
+                user_choice.isdigit() and
+                1 <= int(user_choice) <= len(options)
+            ):
                 current_row = next_rows[int(user_choice) - 1]  # Use next_rows here
                 break
             else:
